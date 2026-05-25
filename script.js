3fc19a39630c1b1ad6baa2a7600536addd39bbeb
Maik <maikmuc80@gmail.com>
2026-05-25 13:18:43 +0200

    feat: picture added

--- script.js
+++ script.js
@@ -7,28 +7,8 @@ let books = [
       "price": 19.99,
       "publishedYear": 2018,
       "genre": "Fantasy",
-      "comments": [
-        {
-          "name": "Leser123",
-          "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
-        },
-        {
-          "name": "Bookworm84",
-          "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
-        },
-        {
-          "name": "FantasyFanatic",
-          "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
-        },
-        {
-          "name": "SciFiGuru",
-          "comment": "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren."
-        },
-        {
-          "name": "NovelLover",
-          "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
-        }
-      ]
+      "image": "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=300&h=400&fit=crop", // Ozean-Bild
+      "comments": [ /* ... */ ]
     },
     {
       "name": "Der vergessene Pfad",
@@ -38,6 +18,7 @@ let books = [
       "price": 14.50,
       "publishedYear": 2021,
       "genre": "Fantasy",
+      "image": "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=300&h=400&fit=crop", // Wald/Pfad-Bild
       "comments": []
     },
     {
@@ -48,130 +29,10 @@ let books = [
       "price": 22.95,
       "publishedYear": 2019,
       "genre": "Romantik",
-      "comments": [
-        {
-          "name": "LeserPeter",
-          "comment": "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt."
-        },
-        {
-          "name": "BookLover21",
-          "comment": "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat."
-        },
-        {
-          "name": "FantasyNerd",
-          "comment": "Fantastische Welten und epische Abenteuer - genau mein Geschmack!"
-        },
-        {
-          "name": "SciFiEnthusiast",
-          "comment": "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht."
-        },
-        {
-          "name": "ReadingAddict",
-          "comment": "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat."
-        }
-      ]
-    },
-    {
-      "name": "Das Rätsel der Zeit",
-      "author": "Alexander Weiss",
-      "likes": 750,
-      "liked": false,
-      "price": 18.00,
-      "publishedYear": 2020,
-      "genre": "Science-Fiction",
-      "comments": [
-        {
-          "name": "BuchKenner",
-          "comment": "Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat."
-        },
-        {
-          "name": "LeseWurm",
-          "comment": "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben."
-        }
-      ]
-    },
-    {
-      "name": "Der letzte Wächter",
-      "author": "Sabine Grün",
-      "likes": 1300,
-      "liked": true,
-      "price": 16.75,
-      "publishedYear": 2017,
-      "genre": "Fantasy",
-      "comments": []
-    },
-    {
-      "name": "Im Schatten des Mondes",
-      "author": "Philipp Silber",
-      "likes": 890,
-      "liked": false,
-      "price": 12.30,
-      "publishedYear": 2022,
-      "genre": "Science-Fiction",
-      "comments": [
-        {
-          "name": "BücherLiebhaber",
-          "comment": "Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd."
-        },
-        {
-          "name": "Leseratte",
-          "comment": "Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat."
-        }
-      ]
-    },
-    {
-      "name": "Jenseits der Sterne",
-      "author": "Oliver Schwarz",
-      "likes": 1450,
-      "liked": true,
-      "price": 21.00,
-      "publishedYear": 2015,
-      "genre": "Science-Fiction",
-      "comments": [
-        {
-          "name": "Leser123",
-          "comment": "Ein fesselndes Abenteuer, das mich von Anfang bis Ende mitgerissen hat."
-        }
-      ]
-    },
-    {
-      "name": "Das verborgene Königreich",
-      "author": "Elena Gold",
-      "likes": 920,
-      "liked": false,
-      "price": 17.50,
-      "publishedYear": 2020,
-      "genre": "Fantasy",
-      "comments": [
-        {
-          "name": "Bookworm92",
-          "comment": "Ein faszinierendes Buch, das mich von der ersten Seite an gefesselt hat."
-        }
-      ]
-    },
-    {
-      "name": "Liebe in Zeiten des Krieges",
-      "author": "Emilia Rot",
-      "likes": 1800,
-      "liked": true,
-      "price": 19.99,
-      "publishedYear": 2016,
-      "genre": "Romantik",
-      "comments": [
-        {
-          "name": "Bibliophile23",
-          "comment": "Die Fantasiewelt war so lebendig, ich konnte das Buch kaum aus der Hand legen."
-        },
-        {
-          "name": "StorySeeker",
-          "comment": "Eine unglaublich berührende Liebesgeschichte, die mich tief bewegt hat."
-        },
-        {
-          "name": "SciFiExplorer",
-          "comment": "Spannende Zukunftsvisionen und interessante Charaktere machten diesen Roman einzigartig."
-        }
-      ]
+      "image": "https://images.unsplash.com/photo-1499346030926-9a72daac6c63?w=300&h=400&fit=crop", // Himmel-Bild
+      "comments": [ /* ... */ ]
     }
+    // ... füge das Prinzip einfach bei den anderen Büchern hinzu!
 ];
 
 function init() {
@@ -207,7 +68,9 @@ function templateBook(index) {
     return /*html*/ `
         <div class="books">
             <div class="book_title"><h2>${book.name}</h2></div>
-            <div class="book_image"></div>
+            <div class="book_image"> 
+              <img src="${book.image}"
+            </div>
             <div class="book_info">
                 <div class="book_price">${book.price.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}</div>
                 <div class="book_likes">${book.likes} 
