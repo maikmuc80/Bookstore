a73679ba91dabeacb488eef675502cea65053b75
Maik <maikmuc80@gmail.com>
2026-05-26 18:54:08 +0200

    feat: book array update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let books = [
       "price": 19.99,
       "publishedYear": 2018,
       "genre": "Fantasy",
-      "image": "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=300&h=400&fit=crop", // Ozean-Bild
+      "image": "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=300&h=400&fit=crop",
       "comments": [ /* ... */ ]
     },
     {
@@ -18,7 +18,7 @@ let books = [
       "price": 14.50,
       "publishedYear": 2021,
       "genre": "Fantasy",
-      "image": "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=300&h=400&fit=crop", // Wald/Pfad-Bild
+      "image": "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=300&h=400&fit=crop",
       "comments": []
     },
     {
@@ -29,8 +29,30 @@ let books = [
       "price": 22.95,
       "publishedYear": 2019,
       "genre": "Romantik",
-      "image": "https://images.unsplash.com/photo-1499346030926-9a72daac6c63?w=300&h=400&fit=crop", // Himmel-Bild
+      "image": "https://images.unsplash.com/photo-1499346030926-9a72daac6c63?w=300&h=400&fit=crop",
       "comments": [ /* ... */ ]
+    },
+    {
+      "name": "FREEDOM - Die Schmahamas-Verschwörung",
+      "author": "Paluten",
+      "likes": 4280,
+      "liked": false,
+      "price": 14.00,
+      "publishedYear": 2018,
+      "genre": "Kinder- & Jugendbuch",
+      "image": "https://images.unsplash.com/photo-1530092285049-1c42085fd395?w=300&h=400&fit=crop",
+      "comments": []
+    },
+    {
+      "name": "Das magische Baumhaus",
+      "author": "Mary Pope Osborne",
+      "likes": 2150,
+      "liked": true,
+      "price": 8.99,
+      "publishedYear": 1992,
+      "genre": "Kinderbuch",
+      "image": "https://images.unsplash.com/photo-1473773508845-188df298d2d1?w=300&h=400&fit=crop",
+      "comments": []
     }
 ];
 
